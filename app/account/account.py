c61907dfedda95b4cb522e18b1ffca7c64523604
abeduer <aurelien.beduer@capgemini.com>
2019-04-22 22:58:41 +0200

    chore: add typings

--- app/account/account.py
+++ app/account/account.py
@@ -1,10 +1,9 @@
-import uuid
 from decimal import *
 
 
 class Account:
-    def __init__(self, balance=Decimal(0)):
+    def __init__(self, balance: Decimal = Decimal(0)) -> None:
         self.balance = balance
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: Decimal) -> None:
         self.balance = self.balance - amount
--- app/client/client.py
+++ app/client/client.py
@@ -2,6 +2,6 @@ from app.account import Account
 
 
 class Client:
-    def __init__(self, name, account=Account()):
+    def __init__(self, name: str, account: Account = Account()) -> None:
         self.name = name
         self.account = account
